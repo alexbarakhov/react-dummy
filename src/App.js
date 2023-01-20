81e6d008c724c1c3c52d45b729c16ac361ba34fb
Alex Barakhov <abarakhov@gmail.com>
2023-01-20 06:23:15 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import Button from './Button';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [count, setCount] = useState(() => {
+    //if you pass the state value as a function version
+    //it will only run the first time it renders
+    console.log('I will run only once');
+    return 0
+  });
 
   function handleClick() {
     console.log("Button was clicked!");
   }
 
+  function rest() {
+    // prev state value
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function sum() {
+    // prev state value
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div className="App">
-        <Button onClick={handleClick}>
-            Click Me
-        </Button>
+        <section>
+          <Button cuandoClick={() => {
+            handleClick()
+          }}>
+              Click Me
+          </Button>
+
+          <button onClick={handleClick}>Click Me 2</button>
+        </section>
+        <section className="countSection">
+          <button onClick={rest}>-</button>
+          <span>{count}</span>
+          <button onClick={sum}>+</button>
+
+        </section>
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,16 +1,36 @@
+import PropTypes from 'prop-types'
 import React, { useState } from 'react';
+import cx from 'classnames'
 
-const Button = ({ onClick, children }) => {
+
+const Button = ({ cuandoClick, children }) => {
     const [clicked, setClicked] = useState(false);
 
+	// Class logic
+	const componentClasses = cx(
+		'testComponentClasses',
+		{'si': true },
+        {'no': false }
+	)
+
     return (
-        <button onClick={() => {
+        <button
+        className={componentClasses}
+        onClick={(e) => {
+
             setClicked(!clicked);
-            onClick();
+            cuandoClick();
+            console.log(e.target)
+
         }}>
             {clicked ? 'Clicked' : children}
         </button>
     );
+}
+
+Button.propTypes = {
+    cuandoClick: PropTypes.func,
+    children: PropTypes.string
 }
 
 export default Button;
